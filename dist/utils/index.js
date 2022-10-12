6d4ba52b43c2afbafc36d7c2c97f6f17cc24a590
Thang Quoc <winofwin292@gmail.com>
2022-10-12 13:35:33 +0700

    - fix cảnh báo khi install

--- dist/utils/index.js
+++ dist/utils/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+require("core-js/modules/es.array.push.js");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
+const path = require("path");
+const pkg = require("./package.json");
 module.exports = {
+    entry: "./src/lib/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        library: pkg.name,
+        libraryTarget: "commonjs2",
+    },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.json$/,
                 loader: "json-loader",
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: {
+                    loader: "url-loader",
+                    options: {
+                        limit: 8192,
+                    },
+                },
             },
         ],
     },
